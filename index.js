1da966db15c515e0a2ca3d1da937817a20062761
PocketDevelopment <45082070+PocketDevelopment@users.noreply.github.com>
2018-12-01 16:14:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client()
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
 bot.login(process.env.TOKEN)
