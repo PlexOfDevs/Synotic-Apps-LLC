cf1084eb9de2caf3b7e06f0e0cf29adbd59240f8
PocketDevelopment <45082070+PocketDevelopment@users.noreply.github.com>
2018-12-01 16:24:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client()
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+bot.on("message", (message) => {
+if(message.content === "~kick") { 
+if(!message.member.roles.some(r=>["PRODUCER"].includes(r.name)) ) if(message.member.id !== config.ownerID) return message.channel.send("🚫 Only Staff Members can kick other Users!") 
+let toKick = message.mentions.members.first() ||message.guild.members.get(args[0]); 
+if(!toKick) return message.channel.send("🚫 Invalid User"); if(!toKick.kickable) return message.channel.send("🚫 Couldn't kick " + `${toKick.user.tag}`); 
+if(toKick.id === config.ownerID) return message.channel.send("🚫 The Bot Developer can't be kicked"); 
+let reason = args.slice(1).join(' '); 
+if(!reason) reason = "No reason provided"; await toKick.kick(reason) .catch(error => message.channel.send(`${error}`)); 
+const kickEmbed = new Discord.RichEmbed() 
+.setAuthor("Synotic Apps LLC.", client.user.avatarURL) 
+.setColor(rainbowhex) 
+.addField("Kicked User", toKick.user.tag) 
+.addField("Kicked by", message.author.tag) 
+.addField("Reason", reason)
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+ message.channel.send(kickEmbed);
+}
 });
 
 bot.login(process.env.TOKEN)
