90abc58af1771dc5b242636efb3dd4c67bc83c52
PocketDevelopment <45082070+PocketDevelopment@users.noreply.github.com>
2018-12-01 14:44:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client()
 
+bot.on("message", (message) => {
+if(message.content === "-help") {
+console.log("Log")
+message.channel.send(`${message.author.username}` + ", I Have Sent You The Commands 📪")
+message.author.send("Commands:\n -ip (TwinFactions )\n -youtube (ModdingTwinz")
+}
+});
+
 bot.login(process.env.TOKEN)
