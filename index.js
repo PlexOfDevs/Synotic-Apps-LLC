52184c2b307df72377a7a91edfe07a8b477c1b68
PocketDevelopment <45082070+PocketDevelopment@users.noreply.github.com>
2018-12-01 16:07:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-const Discord = require("discord.js")
-const bot = new Discord.Client()
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
 
-bot.on("message", (message) => {
-if(message.content === "-help") {
-console.log("Log")
-message.channel.send(`${message.author.username}` + ", I Have Sent You The Commands 📪")
-message.author.send("Commands:\n -ip (TwinFactions )\n -youtube (ModdingTwinz")
-}
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
 });
 
-bot.login(process.env.TOKEN)
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity(`MythicalPE Network`, {type: "STREAMING"});
+
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = process.env.PREFIX;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+});
+
+bot.on("message", (message) => {
+  if(message.content === ".spam") {
+  let channel = message.guild.channels.find(`name`, "spam")
+  if(!channel) return message.channel.send("Channel Blacklisted ❌")
+  if(channel) return message.channel.send(".spam")
+  
+  }
+});
+
+bot.login(process.env.TOKEN);
