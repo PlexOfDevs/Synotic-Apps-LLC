82da5b626372a1630b161bd779bca600cd16c60a
PocketDevelopment <45082070+PocketDevelopment@users.noreply.github.com>
2018-12-02 14:44:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,4 @@ console.log(`${bot.user.username}` + "is online")
 bot.user.setActivity("Syntoic Community", {type: "STREAMING"})
 });
 
-bot.on("message", (message) => {
-if(message.content === "~hub") {
-message.channel.send("hub.stryde.tk : 19132")
-}
-});
-
 bot.login(process.env.TOKEN)
