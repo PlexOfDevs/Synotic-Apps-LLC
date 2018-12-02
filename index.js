4abcaf9284ad9a0abc63a05a9cc8cfe017b37199
PocketDevelopment <45082070+PocketDevelopment@users.noreply.github.com>
2018-12-02 14:43:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client()
 
+console.log(`${bot.user.username}` + "is online")
+
+bot.user.setActivity("Syntoic Community", {type: "STREAMING"})
+});
+
+bot.on("message", (message) => {
+if(message.content === "~hub") {
+message.channel.send("hub.stryde.tk : 19132")
+}
+});
+
 bot.login(process.env.TOKEN)
