d01c38a896ebd4229c538be124e00bf6395aea2a
PocketDevelopment <45082070+PocketDevelopment@users.noreply.github.com>
2018-12-02 14:45:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client()
 
 console.log(`${bot.user.username}` + "is online")
 
-bot.user.setActivity("Syntoic Community", {type: "STREAMING"})
+bot.user.setActivity("Syntoic Community", {type: "WATCHING"})
 });
 
 bot.login(process.env.TOKEN)
